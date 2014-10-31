a50c300a1b6aeb0b3f8070ced4ce26d1efdc2c54
Marc-Andre Lacroix <ma.lacroix@gmail.com>
2014-10-31 13:00:34 -0400

    Moved back the Javascript which were copied to wrong path

--- src/main/webapp/resources/js/js/stimesheet.js
+++
@@ -1,7 +0,0 @@
-
-/**
- * Login : take the select value (the marketplace baseUrl) and redirect to it.
- */
-function login(context) {
-	window.location.href = context + '/j_spring_openid_security_check?openid_identifier=' + $('#marketplace').val() + "/openid/id";
-}
