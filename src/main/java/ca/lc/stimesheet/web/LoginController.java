5a8b57eb5cc9f0c0b054c1bb4a366ef95401beb5
Marc-Andre Lacroix <ma.lacroix@gmail.com>
2014-10-30 22:14:05 -0400

    UI - Simple Login and and welcome page

--- src/main/java/ca/lc/stimesheet/web/LoginController.java
+++ src/main/java/ca/lc/stimesheet/web/LoginController.java
@@ -2,13 +2,20 @@ package ca.lc.stimesheet.web;
 
 import java.io.IOException;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+
+import ca.lc.stimesheet.service.UserSubscriptionService;
 
 @Controller
 @RequestMapping("/login")
 public class LoginController {
+    
+    @Autowired
+    private UserSubscriptionService userSubscriptionService;
 
     @RequestMapping(value="/unauthorized", method=RequestMethod.GET)
     public String unauthorized() throws IOException {
@@ -16,4 +23,12 @@ public class LoginController {
         return "thymeleaf/login/unauthorized";
     }
     
+    @RequestMapping(value="", method=RequestMethod.GET)
+    public String login(Model model) throws IOException {
+        
+        model.addAttribute("marketplaces", userSubscriptionService.findAllPartnerMarketplaces());
+
+        return "thymeleaf/login/login";
+    }
+
 }
